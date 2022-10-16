9f8c873142962793610118181eb8520c8035a347
prabhapatel666 <prabhapatel666@gmail.com>
2022-10-16 21:27:33 +0530

    react-shooping app

---
+++ src/App.js
@@ -0,0 +1,44 @@
+
+import React from "react"
+import "./App.css"
+import Navbar from "./component/Navbar"
+import Home from "./component/Home"
+import Products from "./component/Products"
+import About from "./component/About"
+import Contact from "./component/Contact"
+import Login from "./component/Login"
+import Cart from "./component/Cart"
+ import Checkout from "./component/Checkout"
+import Product from "./component/Product"
+
+
+
+
+import {
+  BrowserRouter as Router,
+  Link,
+  Routes,
+  Route,
+  Switch
+} from "react-router-dom";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      
+      <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/home" element={<Home />}></Route> 
+          <Route exact path="/products" element={<Products />}></Route>
+          <Route exact path="/about" element={<About />}></Route>
+          <Route exact path="/contact" element={<Contact />}></Route>
+          <Route exact path="/login" element={<Login />}></Route>
+          <Route exact path="/cart" element={<Cart />}></Route>
+          <Route exact path="/checkout" element={<Checkout />}></Route>
+          {/* <Route exact path="/Footer" element={<Footer />}></Route>  */}
+          <Route exact path="/products/:id" element={<Product />}></Route>
+        </Routes>
+    </>
+  )
+}
